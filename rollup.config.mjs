b046537725126981d97984cf984a6f0a13ae0676
YUN-CHIEN-LO <yunchienlo@gmail.com>
2023-10-05 17:48:16 +0800

    dev: 新增 側邊欄 tooltip

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,23 +2,35 @@ import typescript from "rollup-plugin-ts";
 import copy from "rollup-plugin-copy";
 import terser from "@rollup/plugin-terser";
 
-export default {
-  input: "src/script/index.ts",
-  output: [
-    {
-      dir: "./dist/scripts",
-      format: "cjs",
-    },
-  ],
-  plugins: [
-    typescript(),
-    copy({
-      targets: [
-        { src: "src/**.html", dest: "dist" },
-        { src: "src/assets/*", dest: "dist/assets" },
-        { src: "src/manifest.json", dest: "dist" },
-      ],
-    }),
-    terser(),
-  ],
-};
+export default [
+  {
+    input: "src/script/index.ts",
+    output: [
+      {
+        dir: "./dist/scripts",
+        format: "cjs",
+      },
+    ],
+    plugins: [
+      typescript(),
+      copy({
+        targets: [
+          { src: "src/**.html", dest: "dist" },
+          { src: "src/assets/*", dest: "dist/assets" },
+          { src: "src/manifest.json", dest: "dist" },
+        ],
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "src/script/page.ts",
+    output: [
+      {
+        dir: "./dist/scripts",
+        format: "cjs",
+      },
+    ],
+    plugins: [typescript(), terser()],
+  },
+];
