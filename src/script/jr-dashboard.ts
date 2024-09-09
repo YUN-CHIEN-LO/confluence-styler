3a117a91ddf7c084655b6d1cdb58fa3b32ed1a42
羅云謙 <chien.lo@syntecclub.com.tw>
2024-09-09 16:32:57 +0800

    wip

--- src/script/jr-dashboard.ts
+++ src/script/jr-dashboard.ts
@@ -11,69 +11,66 @@ function setClockInTime(
   const clockInGadget = document.createElement("div");
   clockInGadget.setAttribute("class", "clock-in-gadget");
 
+  const clockIn = document.createElement("h2");
+  clockIn.setAttribute("id", "clockIn");
+
+  const clockOut = document.createElement("h2");
+  clockOut.setAttribute("id", "clockOut");
+
   const countDown = document.createElement("h2");
   countDown.setAttribute("id", "clockInCountDown");
 
+  const firstClockInTime = ResultValue[0] ? ResultValue[0]["Time"] : null;
+  const [hour, minute] = firstClockInTime.split(":");
+  const expectedClockOutTime =
+    Number(hour) + 9 >= 17 ? `${Number(hour) + 9}:${minute}` : "17:00";
+
+  clockIn.innerText = `最早上班時間：${firstClockInTime}`;
+  clockOut.innerText = `預計下班時間：${expectedClockOutTime}`;
+  countDown.innerText = `倒數剩餘時間：--`;
+
+  clockInGadget.appendChild(clockIn);
+  clockInGadget.appendChild(clockOut);
   clockInGadget.appendChild(countDown);
-
-  // creates a <table> element and a <tbody> element
-  const tbl = document.createElement("table");
-
-  const tblHead = document.createElement("thead");
-  const tblBody = document.createElement("tbody");
-
-  // creates a table row
-  const headRow = document.createElement("tr");
-  const headCell = document.createElement("th");
-  const headCellText = document.createTextNode(`打卡時間`);
-  headCell.appendChild(headCellText);
-  headRow.appendChild(headCell);
-  tblHead.appendChild(headRow);
-
-  // creating all cells
-  for (const element of ResultValue) {
-    // creates a table row
-    const row = document.createElement("tr");
-    row.style.borderBottom = "1px solid #dfe1e6";
-
-    for (let j = 0; j < 1; j++) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      const cell = document.createElement("td");
-      cell.style.textAlign = "center";
-      const cellText = document.createTextNode(`${element["Time"]}`);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-    }
-
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-  }
-
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblHead);
-  tbl.appendChild(tblBody);
-  // sets the border attribute of tbl to '2'
-  tbl.setAttribute("border", "0");
-  tbl.style.borderCollapse = "collapse";
-
-  clockInGadget.appendChild(tbl);
 
   oldElement.replaceWith(clockInGadget);
 
-  const firstClockInTime = ResultValue[0] ? ResultValue[0]["Time"] : null;
-  const [hour, minute] = firstClockInTime.split(":");
-  const expectedClockOutTime = `${Number(hour) + 9}:${minute}`;
+  setInterval(() => {
+    const today = new Date();
+    const clockOutTime = Number(
+      new Date(
+        `${today.getFullYear()}/${
+          today.getMonth() + 1
+        }/${today.getDate()} ${expectedClockOutTime}:00`
+      )
+    );
+    let remainTime = clockOutTime - Number(today);
+    remainTime = Math.ceil(remainTime / 1000);
+    const hour = Math.floor(remainTime / 60 / 60);
+    remainTime -= hour * 60 * 60;
+    const minutes = Math.floor(remainTime / 60);
+    remainTime -= minutes * 60;
+    const seconds = remainTime;
+    const formatter = (time: number): string => `${time}`.padStart(2, "0");
 
-  const countDownElement = document.getElementById("clockInCountDown");
-  if (countDownElement) {
-    countDownElement.innerText = `預計下班時間：${expectedClockOutTime}`;
-  }
+    const countDownElement = document.getElementById("clockInCountDown");
+    if (countDownElement) {
+      countDownElement.innerText = `倒數剩餘時間：${formatter(
+        hour
+      )}:${formatter(minutes)}:${formatter(seconds)}`;
+    }
+  }, 1000);
 }
+
+function getEmpID() {
+  const user = document.getElementById("header-details-user-fullname");
+  if (!user) return;
+  return user.getAttribute("data-username");
+}
+
 async function getClockInTime() {
   const currentDate = new Date().toJSON().slice(0, 10);
-  const EmpID = "10101927";
+  const EmpID = getEmpID();
   const url = `https://ws.syntecclub.com.tw:3790/HRMService.asmx/AT_SKZX_061_GetClockInInfo?EmpID=${EmpID}&BeginDate=${currentDate}&EndDate=${currentDate}`;
   try {
     const response = await fetch(url);
